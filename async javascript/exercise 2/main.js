a10d9f333dfc80b2479fd76aec8e0fcce05d6aa5
dortoo <dorto@live.com>
2020-11-03 01:17:58 -0300

    exercise 2 with axios

--- async javascript/exercise 2/main.js
+++ async javascript/exercise 2/main.js
@@ -1,70 +1,29 @@
-function adicionar() {
-  var input = document.querySelector('div#app input');
-  var minhaPromise = function() {
-    return new Promise(function(resolve, reject) {
-      var xhr = new XMLHttpRequest();
-      xhr.open('GET', 'https://api.github.com/users/'+ input.value + '/repos');
-      xhr.send(null);
-
-      xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4) {
-          if (xhr.status === 200) {
-            resolve(JSON.parse(xhr.responseText));
-          } else {
-            reject('Erro na requisição');
-          }
-        }
-      }
-    });
-  }
-
 var mainElement = document.querySelector('div#app');
 
 var list = document.createElement('ul');
 
 var liElement;
 
-minhaPromise()
+function adicionar() {
+
+  var input = document.querySelector('div#app input');
+
+axios.get('https://api.github.com/users/' + input.value + '/repos')
   .then(function(response) {
-    for(let index = 0; index < response.length; index++) {
+
+    for(let index = 0; index < response.data.length; index++) {
       liElement = document.createElement('li');
-      var text = document.createTextNode(response[index].name);
+      var text = document.createTextNode(response.data[index].name);
       liElement.appendChild(text);
       list.appendChild(liElement);
     }
+
     mainElement.appendChild(list);
-    console.log(response.length);
+    console.log(response.data.length);
+
   })
   .catch(function(error) {
     console.warn(error);
   });
-
 }
 
-
-
-//fazer um onclick no botao que recupera o valor do input e faz a pesquisa
-//diretamente na api do github utilizando ajax.
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var xhr = new XMLHttpRequest();
-
-// xhr.open('GET', 'https://api.github.com/users/dortoo/repos');
-// xhr.send(null);
-
-// xhr.onreadystatechange = function() {
-//   if (xhr.readyState === 4) {
-//     console.log(JSON.parse(xhr.responseText));
-//   }
-// }
